51a3860cf6b7c582d356b38d2104eef78d228bbe
Nolan <nsuduwel@uwo.ca>
2025-03-27 09:06:12 -0400

    Added XPManager Class

---
+++ RPG Game/Assets/Script/Global/XPManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System;
+
+public class XPManager : MonoBehaviour
+{
+    public static XPManager Instance { get; private set; }
+
+    public int currentXP = 0;
+    public int currentLevel = 1;
+    public int xpToNextLevel = 100;
+
+    public event Action<int, int> OnXPUpdated;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void AddXP(int amount)
+    {
+        currentXP += amount;
+        OnXPUpdated?.Invoke(currentXP, currentLevel);
+
+        if (currentXP >= xpToNextLevel)
+        {
+            LevelUp();
+        }
+    }
+
+    private void LevelUp()
+    {
+        currentXP -= xpToNextLevel;
+        currentLevel++;
+        xpToNextLevel = Mathf.RoundToInt(xpToNextLevel * 1.2f);
+
+        Debug.Log($"Leveled Up! New Level: {currentLevel}");
+        OnXPUpdated?.Invoke(currentXP, currentLevel);
+    }
+}
+
