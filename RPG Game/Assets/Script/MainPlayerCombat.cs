86ffd44266b82ab6c97d1f99272f372f7c153477
Hcheem9 <hcheem9@uwo.ca>
2025-04-02 02:37:43 -0400

    Revert "MovementFixedAgain"
    This reverts commit a550a6954a4d9ab2a869f20d7b3b6ab1e42572aa.

--- RPG Game/Assets/Script/MainPlayerCombat.cs
+++ RPG Game/Assets/Script/MainPlayerCombat.cs
@@ -7,14 +7,11 @@ public class MainPlayerCombat : MonoBehaviour
     private Animator animator;
     private int comboCount = 0;
     private float comboTimer = 0f;
-    private float comboWindow = 1f;
-    private CharacterController controller;
+    private float comboWindow = 1f; 
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
         animator = GetComponentInChildren<Animator>();
-        animator.applyRootMotion = true;
     }
 
     void Update()
@@ -51,13 +48,9 @@ public class MainPlayerCombat : MonoBehaviour
         {
             case 1:
                 animator.SetTrigger("Attack1");
-                Vector3 position = animator.rootPosition;
-                controller.Move(position - transform.position);
-                transform.rotation = animator.rootRotation;
                 break;
             case 2:
                 animator.SetTrigger("Attack2");
-                
                 break;
             case 3:
                 animator.SetTrigger("Attack3");
